2a0d32e5ecd8a31658758d6193f318aa4d00c949
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-01-01 15:07:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-      /* add your code here */
+        float[][] values = new float[2][3];
+
+        System.out.println("Number of rows: " + values.length);
+        System.out.println("Number of columns: " + values[0].length);
+
+        System.out.println("Initial array:");
+        printArray(values);
+
+        values[1][2] = 5.5f;
+
+        System.out.println("Array after updating:");
+        printArray(values);
     }
 
 }
+
