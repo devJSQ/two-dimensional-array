6c715b6f3332a280a31c492001c5ea65bd54137a
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-03-16 01:41:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        float[][] values = new float[2][3];
+public class Main8 {
+  public static void main(String[] args) {
+      float[][] values = new float[2][3];
 
-        System.out.println("Number of rows: " + values.length);
-        System.out.println("Number of columns: " + values[0].length);
+      System.out.println("Number of rows: " + values.length);
+      System.out.println("Number of columns: " + values[0].length);
 
-        System.out.println("Initial array:");
-        printArray(values);
+      System.out.println("Initial array:");
+      printArray(values);
 
-        values[1][2] = 5.5f;
+      values[1][2] = 5.5f;
 
-        System.out.println("Array after updating:");
-        printArray(values);
+      System.out.println("Array after updating:");
+      printArray(values);
+  }
+
+  private static void printArray(float [][] thearray){
+    for(int x = 0; x < thearray.length; x++){
+      System.out.println(x + "row");
+      for (int i = 0; i < thearray[x].length; i ++){
+        System.out.println(thearray[x][i]);
+      }
     }
+  }
 
 }
 
